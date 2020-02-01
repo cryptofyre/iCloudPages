6e1186d32c6c9ecb6f2590e8c9b4a3d9095ca90f
iiFir3z <33162551+iiFir3z@users.noreply.github.com>
2020-02-01 14:43:37 -0800

    Add source code documentation and files.
    All the files are belong to everyone

---
+++ iCloud Pages/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The webview only stores your Password and stuff on your computer.
+// Background page redirect shit. Makes WebView work with iCloud Pages.
+
+namespace iCloud_Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NewPageWindow(WebView sender, WebViewNewWindowRequestedEventArgs args)
+        {
+            // webview creation on page creation.
+            Pages.Navigate(args.Uri);
+            args.Handled = true;
+        }
+    }
+}
